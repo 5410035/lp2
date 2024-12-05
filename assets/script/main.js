0c7e6f21a2d91c202641f9ece304ef8e843b1677
5410035 <5410035sryuji331@gmail.com>
2024-12-05 18:57:24 +0900

    241205_メンタリング前

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,12 @@
-// ハンバーガーメニュー
+// カルーセル
+$('.voice__slick').slick({
+  infinite: true,
+  dots: true,
+  arrows: true,
+  prevArrow: '<img src="./assets/image/voice_left_arrow_sp_01.svg" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="./assets/image/voice_right_arrow_sp_01.svg" class="slide-arrow next-arrow">',
+  fade: true
+});
+
+// カルーセル
+
