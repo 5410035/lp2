7d0798488575a1bc8acfddff55ec3623bd90bb1a
5410035 <5410035sryuji331@gmail.com>
2025-01-10 22:36:21 +0900

    250107_修正稿と既存サイトの修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,43 +1,43 @@
 
 // アコーディオン
 $('.question__accordion-title').on('click', function (e) {
-  // .accordion-contentを表示・非表示
-  $(this).next().slideToggle();
-  $(this).find(".question__accordion-title-triangle").toggleClass("open", 300);
+    // .accordion-contentを表示・非表示
+    $(this).next().slideToggle();
+    $(this).find(".question__accordion-title-triangle").toggleClass("open", 300);
 });
 
 // アコーディオン お客様の声
 $(".voice__slick").slick({
-  arrows: true,
-  autoplay: false,
-  adaptiveHeight: true,
-  dots: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  fade: false,
-  prevArrow: '<img src="./assets/image/voice_left_arrow_sp.svg" class="slide-arrow prev-arrow" alt="前へ戻る">',
-  nextArrow: '<img src="./assets/image/voice_right_arrow_sp.svg" class="slide-arrow next-arrow"　alt="次へ進む">',
-  responsive: [
-    {
-      breakpoint: 767.9,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
+    arrows: true,
+    autoplay: false,
+    adaptiveHeight: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    fade: false,
+    prevArrow: '<img src="./assets/image/voice_left_arrow_sp.svg" class="slide-arrow prev-arrow" alt="前へ戻る">',
+    nextArrow: '<img src="./assets/image/voice_right_arrow_sp.svg" class="slide-arrow next-arrow"　alt="次へ進む">',
+    responsive: [
+        {
+            breakpoint: 767.9,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
 });
 
 $(document).ready(function () {
-  const $topuparrow = $('.topuparrow');
-  const scrollThreshold = 200; // 指定値 (スクロール距離)
+    const $topuparrow = $('.topuparrow');
+    const scrollThreshold = 200; // 指定値 (スクロール距離)
 
-  // スクロール時の処理
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > scrollThreshold) {
-      $topuparrow.fadeIn(); // フェードイン
-    } else {
-      $topuparrow.fadeOut(); // フェードアウト
-    }
-  });
+    // スクロール時の処理
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > scrollThreshold) {
+            $topuparrow.fadeIn(); // フェードイン
+        } else {
+            $topuparrow.fadeOut(); // フェードアウト
+        }
+    });
 });
